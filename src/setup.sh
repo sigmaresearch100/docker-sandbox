fc0e1816a72dc966cc24e016d7d348b67d5ac191
sigmaresearch100 <136125754+sigmaresearch100@users.noreply.github.com>
2023-06-12 22:03:08 +0800

    Update setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -2,12 +2,5 @@
 
 set -e
 
-cat > /etc/apk/repositories << EOF; $(echo)
-
-https://dl-cdn.alpinelinux.org/alpine/v$(cut -d'.' -f1,2 /etc/alpine-release)/main/
-https://dl-cdn.alpinelinux.org/alpine/v$(cut -d'.' -f1,2 /etc/alpine-release)/community/
-https://dl-cdn.alpinelinux.org/alpine/edge/testing/
-
-EOF
-
-apk update
+apt-get update
+apt-get install -y vim
