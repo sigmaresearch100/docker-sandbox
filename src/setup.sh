44f469de8304860823c0fc8c9b1981406e4950a8
sigmaresearch100 <136125754+sigmaresearch100@users.noreply.github.com>
2023-06-12 20:57:44 +0800

    Update setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -2,4 +2,13 @@
 
 set -e
 
+cat > /etc/apk/repositories << EOF; $(echo)
+
+https://dl-cdn.alpinelinux.org/alpine/v$(cut -d'.' -f1,2 /etc/alpine-release)/main/
+https://dl-cdn.alpinelinux.org/alpine/v$(cut -d'.' -f1,2 /etc/alpine-release)/community/
+https://dl-cdn.alpinelinux.org/alpine/edge/testing/
+
+EOF
+
+apk update
 apk add --no-cache R
